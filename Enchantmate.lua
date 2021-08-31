8b3b8bfc640501091c311a08e7650a1e4de29bc1
stpain <sam.pain@live.co.uk>
2021-09-01 00:12:55 +0100

    added events
    added event registration
    reworked app table

--- Enchantmate.lua
+++ Enchantmate.lua
@@ -1,11 +1,13 @@
 
 
+local colours = {
+    Blue = CreateColor(0.1, 0.58, 0.92, 1),
+    Orange = CreateColor(0.79, 0.6, 0.15, 1),
+    Yellow = CreateColor(1.0, 0.82, 0, 1),
+}
 
 
-local app = {}
-
-
-local enchantSlots = {
+local characterInvSlots = {
     { invSlot = "CharacterBackSlot", enchants = "Enchant Cloak" },
     { invSlot = "CharacterChestSlot", enchants = "Enchant Chest" },
     { invSlot = "CharacterWristSlot", enchants = "Enchant Bracer" },
@@ -15,73 +17,95 @@ local enchantSlots = {
     { invSlot = "CharacterFinger1Slot", enchants = "Enchant Ring" },
     { invSlot = "CharacterMainHandSlot", enchants = { "Enchant Weapon", "Enchant 2H Weapon" } },
     { invSlot = "CharacterSecondaryHandSlot", enchants = { "Enchant Weapon", "Enchant Shield" } },
-    --{ invSlot = "CharacterRangedSlot", enchants = "Enchant Weapon" },
 }
 
-local function isEnchanter()
-    for i = 1, GetNumSkillLines() do
-        local skill, _, _, level, _, _, _, _, _, _, _, _, _ = GetSkillLineInfo(i)
-        if skill == "Enchanting" then
-            return true;
-        end
-    end
-    return false;
-end
+local app = {
 
-local function getAvailableEnchantsForSlot(slot)
-    CraftFrame:SetAlpha(0)
-    CastSpellByName("Enchanting")
-    local enchants = {}
-    for i = 1, GetNumCrafts() do
-        local craftName, craftSubSpellName, craftType, numAvailable, isExpanded, trainingPointCost, requiredLevel = GetCraftInfo(i)
-        if (craftType == "optimal" or craftType == "medium" or craftType == "easy" or craftType == "trivial") then -- this is to make sure we only get enchants the player knows
-            local _, _, _, _, _, _, itemID = GetSpellInfo(craftName)
-            local numReagents = GetCraftNumReagents(i);
-            local reagents = {}
-            if numReagents > 0 then
-                for j = 1, numReagents do
-                    local _, _, reagentCount = GetCraftReagentInfo(i, j)
-                    local reagentLink = GetCraftReagentItemLink(i, j)
-                    if reagentLink and reagentCount then
-                        table.insert(reagents, {
-                            link = reagentLink,
-                            count = reagentCount,
-                        })
-                    end
-                end
-            end
-            local enchantFound = false;
-            if type(slot.enchants) == "table" then
-                for _, enchant in ipairs(slot.enchants) do
-                    if craftName:find(enchant) then
-                        enchantFound = true
-                    end
-                end
-            else
-                if craftName:find(slot.enchants) then
-                    enchantFound = true;
-                end
-            end
-            if enchantFound then
-                table.insert(enchants, {
-                    name = craftName,
-                    count = numAvailable,
-                    slot = slot.invSlot,
-                    itemID = itemID,
-                    typeID = craftType,
-                    reagents = reagents, 
-                })
+    isEnchanter = function()
+        for i = 1, GetNumSkillLines() do
+            local skill, _, _, level, _, _, _, _, _, _, _, _, _ = GetSkillLineInfo(i)
+            if skill == "Enchanting" then
+                return true;
             end
         end
-    end
-    C_Timer.After(0.1, function()
-        CraftFrameCloseButton:Click()
-        CraftFrame:SetAlpha(1)
-    end)
-    table.sort(enchants, function(a,b)
-        return a.count > b.count
-    end)
-end
+        return false;
+    end,
+
+    getAvailableEnchantsForSlot = function(slot)
+        CraftFrame:SetAlpha(0)
+        CastSpellByName("Enchanting")
+        local enchants = {}
+        for i = 1, GetNumCrafts() do
+            local craftName, craftSubSpellName, craftType, numAvailable, isExpanded, trainingPointCost, requiredLevel = GetCraftInfo(i)
+            if (craftType == "optimal" or craftType == "medium" or craftType == "easy" or craftType == "trivial") then -- this is to make sure we only get enchants the player knows
+                local _, _, _, _, _, _, itemID = GetSpellInfo(craftName)
+                local numReagents = GetCraftNumReagents(i);
+                local reagents = {}
+                if numReagents > 0 then
+                    for j = 1, numReagents do
+                        local _, _, reagentCount = GetCraftReagentInfo(i, j)
+                        local reagentLink = GetCraftReagentItemLink(i, j)
+                        if reagentLink and reagentCount then
+                            table.insert(reagents, {
+                                link = reagentLink,
+                                count = reagentCount,
+                            })
+                        end
+                    end
+                end
+                local enchantFound = false;
+                if type(slot.enchants) == "table" then
+                    for _, enchant in ipairs(slot.enchants) do
+                        if craftName:find(enchant) then
+                            enchantFound = true
+                        end
+                    end
+                else
+                    if craftName:find(slot.enchants) then
+                        enchantFound = true;
+                    end
+                end
+                if enchantFound then
+                    table.insert(enchants, {
+                        name = craftName,
+                        count = numAvailable,
+                        slot = slot.invSlot,
+                        itemID = itemID,
+                        typeID = craftType,
+                        reagents = reagents, 
+                    })
+                end
+            end
+        end
+        C_Timer.After(0.1, function()
+            CraftFrameCloseButton:Click()
+            CraftFrame:SetAlpha(1)
+        end)
+        table.sort(enchants, function(a,b)
+            return a.count > b.count
+        end)
+        return enchants;
+    end,
+
+    scanPlayerBags = function(self)
+
+    end,
+
+    load = function(self)
+        if not self.isEnchanter() then
+            return;
+        end
+        for _, slot in ipairs(characterInvSlots) do
+            local b = CreateFrame("BUTTON", "CraftCreateButton_"..slot.invSlot, PaperDollItemsFrame, "Enchantmate_InvSlotButton")
+            b:SetPoint("BOTTOMRIGHT", slot.invSlot, "BOTTOMRIGHT", 5, -5)
+            b.slot = slot
+        end
+        self.craftMenu = Enchantmate_CraftMenu;
+    end,
+}
+
+
+
 
 
 Enchantmate_EnchantButtonMixin = {}
@@ -105,15 +129,42 @@ end
 
 Enchantmate_InvSlotButtonMixin = {}
 function Enchantmate_InvSlotButtonMixin:OnClick()
-
+    local button = self;
+    app.getAvailableEnchantsForSlot(button.slot)
+    app.craftMenu:Show()
+    app.craftMenu:ClearAllPoints()
+    app.craftMenu:SetPoint("LEFT", button, "RIGHT", -5, 0)
 end
 
 
 
-EnchantmateMixin = {}
-function EnchantmateMixin:OnEnter()
+Enchantmate_CraftMenuMixin = {}
+--Enchantmate_CraftMenuMixin.
+function Enchantmate_CraftMenuMixin:OnLoad()
+    self:RegisterEvent("ADDON_LOADED")
+end
+function Enchantmate_CraftMenuMixin:OnEvent(event, ...)
+    if ... == "Enchantmate" then
+        self:UnregisterEvent("ADDON_LOADED")
+        app:load()
+    end
+end
+function Enchantmate_CraftMenuMixin:OnEnter()
 
 end
-function EnchantmateMixin:OnLeave()
+function Enchantmate_CraftMenuMixin:OnLeave()
     self:Hide()
+end
+
+
+
+Enchantmate_DisenchantMenuMixin = {}
+--Enchantmate_CraftMenuMixin.
+function Enchantmate_DisenchantMenuMixin:OnLoad()
+    self:RegisterEvent("BAG_UPDATE_DELAYED")
+end
+function Enchantmate_DisenchantMenuMixin:OnEvent(event, ...)
+    if event == "BAG_UPDATE_DELAYED" then
+        
+    end
 end
